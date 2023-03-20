4b439c997d84f443978b88ed113ef2e7add94236
mesh <meshack.korir@student.moringaschool.com>
2023-03-20 00:38:59 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Meshack", "Korir", "Kimwetich"];
+const eventName = "Graduation"
+
+function writeCards(names, occasion) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return messages;
+  }
+
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
